cec8265325cae3fbe62bf96a7993518ea5d2c96a
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-03-04 14:22:16 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -1,46 +1,19 @@
 import './sass/styles.scss';
 import moviesService from './js/fetchMovies';
 import addCardTpl from './js/markup';
+import getItems from './js/getItems';
+import pagination from './js/pagination';
 
-moviesService.fetchTrendingMovies().then(results => {
-  console.log(results);
-  const genres = [
-    { id: 28, name: 'Action' },
-    { id: 12, name: 'Adventure' },
-    { id: 16, name: 'Animation' },
-    { id: 35, name: 'Comedy' },
-    { id: 80, name: 'Crime' },
-    { id: 99, name: 'Documentary' },
-    { id: 18, name: 'Drama' },
-    { id: 10751, name: 'Family' },
-    { id: 14, name: 'Fantasy' },
-    { id: 36, name: 'History' },
-    { id: 27, name: 'Horror' },
-    { id: 10402, name: 'Music' },
-    { id: 9648, name: 'Mystery' },
-    { id: 10749, name: 'Romance' },
-    { id: 878, name: 'Science Fiction' },
-    { id: 10770, name: 'TV Movie' },
-    { id: 53, name: 'Thriller' },
-    { id: 10752, name: 'War' },
-    { id: 37, name: 'Western' },
-  ];
-  const items = results.map(
-    ({ title, id, poster_path, genre_ids, release_date, vote_average }) => {
-      const year = release_date.slice(0, 4);
-      const rating = String(vote_average).padEnd(3, '.0');
-      let genresNames = genre_ids.map(backebdId =>
-        genres.find(({ id }) => id === backebdId),
-      );
-      genresNames = [...genresNames.map(({ name }) => name)];
-      console.log(genresNames);
-      const properties = { id, poster_path, genresNames, year, rating, title };
-      return properties;
-    },
-  );
-  console.log(items);
-  addCardTpl(items);
-});
+pagination.paginationTrendingMovies();
+
+// moviesService.fetchTrendingMovies().then(results => {
+//   console.log(results);
+
+//   const items = getItems(results);
+//   console.log(items);
+
+//   addCardTpl(items);
+// });
 
 // moviesService.searchMovies();
 // moviesService.getFullInfo();
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 const refs = {
   filmsList: document.querySelector('#films-section'),
+  dataContainer: document.querySelector('#dataContainer'),
 };
 
 export default refs;
