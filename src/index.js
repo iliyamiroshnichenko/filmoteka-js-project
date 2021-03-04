833297e75e944dcbfcb6f07e94b96c0e78cdb37a
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-03-04 02:11:18 +0200

    cardTpl

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './sass/styles.scss';
 import moviesService from './js/fetchMovies';
+import addCardTpl from './js/markup';
 
-// moviesService.fetchTrendingMovies();
+moviesService.fetchTrendingMovies().then(results => {
+  console.log(results);
+  const items = results.map(
+    ({ title, id, poster_path, genre_ids, release_date, vote_average }) => {
+      const year = release_date.slice(0, 4);
+      const rating = String(vote_average).padEnd(3, '.0');
+      const properties = { id, poster_path, genre_ids, year, rating, title };
+      return properties;
+    },
+  );
+  console.log(items);
+  addCardTpl(items);
+});
+
+//   .then(results => {
+//   console.log(results);
+//   addCardTpl(results);
+// });
 // moviesService.searchMovies();
 // moviesService.getFullInfo();
--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,6 +1,6 @@
 const basicUrl = 'https://api.themoviedb.org/3/';
 const key = '8e2d6c50ec8673fce37d0988f16fea97';
-const trendingMovieUrl = `${basicUrl}trending/movie/day?api_key=${key}`;
+const trendingMovieUrl = `${basicUrl}trending/movie/day?api_key=${key}&per_page=12`;
 const searchMovieUrl = `${basicUrl}search/movie/?api_key=${key}`;
 
 export default {
@@ -8,15 +8,22 @@ export default {
   _movieId: 321528,
 
   fetchTrendingMovies() {
-    return fetch(trendingMovieUrl)
-      .then(response => {
-        if (response.ok) return response.json();
-        throw new Error('Error Fetching data');
-      })
-      .then(({ results }) => console.log(results))
-      .catch(error => {
-        console.log(error);
-      });
+    return (
+      fetch(trendingMovieUrl)
+        .then(response => {
+          if (response.ok) return response.json();
+          throw new Error('Error Fetching data');
+        })
+        .then(({ results }) => {
+          return results;
+        })
+        // .then(({ id, poster_path, genre_ids, release_date, vote_average }) => {
+        //   console.log(id, poster_path, genre_ids, release_date, vote_average);
+        // })
+        .catch(error => {
+          console.log(error);
+        })
+    );
   },
   searchMovies() {
     return fetch(`${searchMovieUrl}&query=${this.query}`)
@@ -24,7 +31,9 @@ export default {
         if (response.ok) return response.json();
         throw new Error('Error Fetching data');
       })
-      .then(({ results }) => console.log(results))
+      .then(({ results }) => {
+        return results;
+      })
       .catch(error => {
         console.log(error);
       });
