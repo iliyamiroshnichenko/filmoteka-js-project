fe1afd81bcb0ad74c3949d4f970e4e3a34e32271
Iliya <iliyamiroshnichenko88@gmail.com>
2021-03-11 23:01:26 +0200

    some updates

--- src/index.js
+++ src/index.js
@@ -3,29 +3,7 @@ import './js/modal';
 import './js/theme';
 import './js/anchorTop';
 import './js/searchMovies';
-import pagination from './js/pagination';
-import refs from './js/refs';
+import './js/pagination';
+import './js/refs';
 import './js/footer';
 import './js/modal-team';
-// import moviesService from './js/fetchMovies';
-// import switchPage from './js/switchPages';
-// import addCardTpl from './js/markup';
-// import modalTpl from './templates/modal.hbs';
-// import getItems from './js/getItems';
-
-// refs.searchForm.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const form = event.currentTarget;
-//   const searchQuery = form.elements.query.value;
-//   const trimSearchQuery = searchQuery.trim();
-//   // debugger
-//   if (!trimSearchQuery) {
-//     refs.error.classList.remove('visually-hidden');
-//     return;
-//   } else {
-//     refs.error.classList.add('visually-hidden');
-//     refs.filmsList.innerHTML = '';
-//     pagination.paginationSearchMovies(searchQuery);
-//   }
-//   form.reset();
-// });
