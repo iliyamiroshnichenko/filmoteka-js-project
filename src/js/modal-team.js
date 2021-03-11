9d6d64bc24fa89291a647882fe60c0cb5ab65f15
Iliya <iliyamiroshnichenko88@gmail.com>
2021-03-11 19:58:46 +0200

    fix some bugs

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,19 +1,22 @@
 import teamModalTpl from '../templates/team.hbs';
 import footerTpl from '../templates/footer.hbs';
 import team from '../js/team';
+import { showSpinnerInModal, hideSpinnerInModal } from './markup';
 
 const refs = {
-    openTeam: document.querySelector('.footer__link'),
-    body: document.querySelector('body'),
-    list: document.querySelector('.team-list') 
-}
+  openTeam: document.querySelector('.footer__link'),
+  body: document.querySelector('body'),
+  team: document.querySelector('.team'),
+};
 
 refs.openTeam.addEventListener('click', onOpenModal);
 
-function onOpenModal(event) { 
-    refs.body.classList.add('show-modal');
-    renderTeamModal(team);
-    window.addEventListener('keydown', onKeyPress);
+function onOpenModal(event) {
+  event.preventDefault();
+  hideSpinnerInModal();
+  refs.body.classList.add('show-modal');
+  renderTeamModal(team);
+  window.addEventListener('keydown', onKeyPress);
 }
 
 function onKeyPress(event) {
@@ -27,10 +30,9 @@ function onCloseModal() {
   window.removeEventListener('keydown', onKeyPress);
 }
 
-
 function renderTeamModal(data) {
-    const markup = teamModalTpl(team);
-    refs.list.insertAdjacentHTML('beforeend', markup)
+  const markup = teamModalTpl(team);
+  refs.team.insertAdjacentHTML('beforeend', markup);
 }
 
 function onBackdropClick(e) {
